4ac50fac6bf5c0dbb708117f2ad47eb378aad471
ruzen42 <malledefer@gmail.com>
2025-05-07 11:16:49 +0500

    change color

--- matrix.js
+++ matrix.js
@@ -5,7 +5,7 @@ canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
 const chars = '我叫伊拉我是学生我在大学学习外语我会说英语和汉语因为我和我的朋友学习汉语所以我们要在假期去中国了解中国文化练习汉语口语安德烈说我们在中国可以报语言学校1佩佳找到了一所学校于是我买了飞机票卡佳在宾馆订了两间房间拿到签证以后我们就出发今天（二月二日星期一是我们在北京的第一天ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()_+-=[]{}|;:,.<>?йцукенгшщзхъфывапярчисмотльдбжюэ.';
-const fontSize = 10;
+const fontSize = 20;
 const columns = canvas.width / fontSize;
 const drops = [];
 
@@ -17,7 +17,7 @@ function draw() {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = '#00ff88';
+    ctx.fillStyle = '#00fc00';
     ctx.font = fontSize + 'px monospace';
 
     for (let i = 0; i < drops.length; i++) {
